51e7904adb34e9a353f264870965ccaec15e4a9c
aboba <klopot.vadim.1337@gmail.com>
2026-03-11 20:32:55 +0300

    Удалён модуль игровой логики

--- src/core/game_logic.py
+++
@@ -1,5 +0,0 @@
-"""Базовая игровая логика шахматной партии."""
-
-class GameLogic:
-    def is_checkmate(self, legal_moves: int, king_in_check: bool) -> bool:
-        return legal_moves == 0 and king_in_check
